6cd7f6d815f9cbeaa1ba44331c067d05c9a570f0
Matthew <tanmatt804@gmail.com>
2022-05-15 14:44:57 -0400

    fixed controller fetchuser import

--- routes/douladoRouter.js
+++ routes/douladoRouter.js
@@ -1,10 +1,10 @@
 const router = require('express').Router(); 
 const pool = require('../db/db')
-const { getAllUsers, fetchPost, makeAPost }  = require('../controller/douladoController');
+const { fetchUsers, fetchPost, makeAPost }  = require('../controller/douladoController');
 
 
 
-  router.get('/users', getAllUsers)
+  router.get('/users', fetchUsers)
   
   router.get('/post', fetchPost)
 
